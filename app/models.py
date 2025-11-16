f71e79bc5ea8c647c07c5aaef6f71f36d2edcb2a
CHMOSHN <andreyvlsv2009@gmail.com>
2025-11-16 03:34:20 +0300

    Создал бдшки + пофиксил баги

--- app/models.py
+++ app/models.py
@@ -3,41 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vulnerabils.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_object('config.DevelopementConfig')
 
 db = SQLAlchemy(app)
+
 # юзеры для авторизации, если отменяем ее то делитнуть
-class users(db.model):
+class users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    scans = db.relationship('scan', backref='users', lazy=True)
+    scans = db.relationship('scans', backref='users', lazy=True)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 # сканы для подачи кода в сайт и запуска анализа коода юзера
-class scans(db.model):
+class scans(db.Model):
     __tablename__ = 'scans'
+    vulnerability=db.relationship('vulnerabilities', backref='scans', lazy=True)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    date_scan = db.Column(db.DateTime, nullable = False, dafault=datetime.utcnow)
+    date_scan = db.Column(db.DateTime, nullable = False, default=datetime.now())
     code_language =db.Column(db.String(20), nullable=False) 
     code = db.Column(db.Text, nullable=False)
-    vulnerability=db.Relationship('vulnerabilities', backref='scans', lazy=True)
+    
 # уязвимости для вывода результатов
-class vulnerabilities(db.model):
+class vulnerabilities(db.Model):
     __tablename__='vulnerabilities'
-    id = db.column(db.Integer, primary_key=True, autoincrement=True)
-    scan_id = db.Column(db.Integer, db.Foreignkey('scans.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    scan_id = db.Column(db.Integer, db.ForeignKey('scans.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
     line = db.Column(db.Integer, nullable=False)
     code_snippet = db.Column(db.Text)
     type = db.Column(db.String(46), nullable=False)
     risk_level = db.Column(db.String(25), nullable=False)
-# создание бд при первом запуске
-@app.before_first_request
-def before_first_request():
-    db.create_all()
-    db.session.add(user(username='', password='')) # можно вписать свой логин и пароль как админа
-# надо будет сделать запуск с конфига
+
--- config.py
+++ config.py
@@ -7,12 +7,10 @@ app_dir = path.abspath(path.dirname(__file__))
 
 #Базовый конфиг
 class BaseConfig:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SECRET_KEY: str = r'XP;W/\vD*BOQ_ieHYfEl1GJ!2}Z5[?S:A.7ykd6nL3c8qwpN+$bjt])MV#0z' #! Секретный ключ сгенерированный лично мной, не палить, пока не нужен
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     
 #! Конфиг для разработки, делитнуть на релизе!
 class DevelopementConfig(BaseConfig):
-    DEBUG = True
-    
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///vulns.db'
-    
-    
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///default.db'
