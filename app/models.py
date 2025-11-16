7a0d7d5b145a9d756b50b7deb035f4ea62c623d0
Binary101010 <gosling712@gmail.com>
2025-11-16 16:57:08 +0300

    update models
    исправил связи(relationship) и опечатки, улучшил datetime, изменил названия таблиц

--- app/models.py
+++ app/models.py
@@ -1,32 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopementConfig')
+app.config.from_object('config.DevelopmentConfig')
 
 db = SQLAlchemy(app)
 
 # юзеры для авторизации, если отменяем ее то делитнуть
-class users(db.Model):
+class user(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    scans = db.relationship('scans', backref='users', lazy=True)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    username = db.Column(db.String(32), nullable=False, unique=True)
+    password = db.Column(db.String(32), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    scans = db.relationship('scan', backref='users', lazy=True)
 # сканы для подачи кода в сайт и запуска анализа коода юзера
-class scans(db.Model):
+class scan(db.Model):
     __tablename__ = 'scans'
-    vulnerability=db.relationship('vulnerabilities', backref='scans', lazy=True)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    date_scan = db.Column(db.DateTime, nullable = False, default=datetime.now())
-    code_language =db.Column(db.String(20), nullable=False) 
+    date_scan = db.Column(db.DateTime, nullable = False, default=lambda: datetime.now(timezone.utc))
+    code_language = db.Column(db.String(50), nullable=False) 
     code = db.Column(db.Text, nullable=False)
+    vulnerabilities = db.relationship('vulnerability', backref='scans', lazy=True)
     
-# уязвимости для вывода результатов
-class vulnerabilities(db.Model):
+# найденные уязвимости для вывода результатов
+class vulnerability(db.Model):
     __tablename__='vulnerabilities'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     scan_id = db.Column(db.Integer, db.ForeignKey('scans.id'), nullable=False)
@@ -34,8 +34,9 @@ class vulnerabilities(db.Model):
     description = db.Column(db.Text)
     line = db.Column(db.Integer, nullable=False)
     code_snippet = db.Column(db.Text)
-    type = db.Column(db.String(46), nullable=False)
+    vulnerability_type = db.Column(db.String(50), nullable=False)
     risk_level = db.Column(db.String(25), nullable=False)
+    scan = db.relationship('scan', backref='vulnerabilities')
 
 with app.app_context():
     db.create_all()
