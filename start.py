2abe59eae95050018177b1ba156ab203e3cbc09e
CHMOSHN <andreyvlsv2009@gmail.com>
2025-11-16 03:39:53 +0300

    Фиксанул оставшиеся баги

--- start.py
+++ start.py
@@ -1,5 +1,18 @@
 #Импорты
 from app import app, db
 
+app.config.from_object('config.DevelopementConfig')
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("Таблицы созданы!")
+        
+        #Проверка созданных таблиц
+        from sqlalchemy import inspect
+        inspector = inspect(db.engine)
+        tables = inspector.get_table_names()
+        print(f"Список созданных таблиц: {tables}")
+
 if __name__ == '__main__':
+    init_db()
     app.run()
