5595737f8bc578e61bd542bfcf1172ccf646a5c1
Binary101010 <gosling712@gmail.com>
2025-11-19 02:10:48 +0300

    Сделан роут /user/scan

--- app/views.py
+++ app/views.py
@@ -1,76 +1,111 @@
-#Файл для всех роутов
-#Импорты
+# Файл для всех роутов
+# Импорты
 from app import app, db
 from flask import render_template, request, redirect, url_for, session, flash
-from flask_login import login_required, login_user,current_user, logout_user
+from flask_login import login_required, login_user, current_user, logout_user
 from werkzeug.security import check_password_hash
 from .models import User
 import sqlalchemy as sql
 from .forms import LoginForm
-#Базовая страница
-@app.route('/')
+from .services import run_scanner_service
+
+
+# Базовая страница
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/user/')
+
+@app.route("/user/")
 def profile():
-    return render_template('profile.html')
+    return render_template("profile.html")
 
-#Логин
-@app.route('/user/login/', methods=['GET', 'POST'])
+
+# Логин
+@app.route("/user/login/", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('scan'))
-    
-    form = LoginForm()    
+        return redirect(url_for("scan"))
+
+    form = LoginForm()
     if form.validate_on_submit():
-        user = db.session.scalar(sql.select(User).where(User.username == form.username.data))
-        
+        user = db.session.scalar(
+            sql.select(User).where(User.username == form.username.data)
+        )
+
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))  
-    return render_template('login.html', title='Login', form=form)
-    
-@app.route('/user/registration', methods=['GET', 'POST'])
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/user/registration", methods=["GET", "POST"])
 def registration():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
-            flash('Пользователь с таким именем уже существует!')
-            return redirect(url_for('registration'))
-        
+            flash("Пользователь с таким именем уже существует!")
+            return redirect(url_for("registration"))
+
         new_user = User(username=username)
         new_user.set_password(password)
-        
+
         # Сохраняем в БД
         db.session.add(new_user)
         db.session.commit()
-        
+
         # Автоматически логиним пользователя
         login_user(new_user)
-        flash('Регистрация успешна!')
-        return redirect('/')    
-    return render_template('register.html')
+        flash("Регистрация успешна!")
+        return redirect("/")
+    return render_template("register.html")
 
-        
-#Сам сканнер
-@app.route('/user/scan', methods=['POST', 'GET'])
+
+# Сам сканнер
+@app.route("/user/scan", methods=["POST"])
+@login_required  # проверка на авторизацию
 def scan():
-    return render_template('scan.html')
+    # запуск сканера и ввод
+    try:
+        scanner = run_scanner_service()
+        code = request.form.get("code")
+        repo = request.form.get("repo_url")
+        # проверка входных данных
+        if code:
+            result = scanner.run_code_scan(code)
+        elif repo:
+            result = scanner.run_repo_scan(repo)
+        else:
+            flash("Укажите код или URL репозитория на GitHub")
+            return redirect(url_for("index"))
+        return render_template("results.html", result=result)
 
-#Результаты скана
-@app.route('/user/results', methods=['POST'])
+    # если ошибка то кинет на index
+    except ValueError as e:
+        flash(f"Ошибка конфигурации: {str(e)}")
+        return redirect(url_for("index"))
+    except RuntimeError as e:
+        flash(f"Ошибка сканирования: {str(e)}")
+        return redirect(url_for("index"))
+    except Exception as e:
+        flash(f"Ошибка: {str(e)}")
+        return redirect(url_for("index"))
+
+
+# Результаты скана
+@app.route("/user/results", methods=["POST"])
 def results():
     return "Results page"
 
-@app.route('/user/logout')
+
+@app.route("/user/logout")
 @login_required
 def logout():
     logout_user()
-    flash('Вы вышли из системы')
-    return redirect(url_for('index'))
+    flash("Вы вышли из системы")
+    return redirect(url_for("index"))
