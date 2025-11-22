e5d06335af8a83407d5076010a263d3aa56d5177
Binary101010 <gosling712@gmail.com>
2025-11-22 21:55:33 +0300

    изменение маршрута /profile/scan под CLI

--- app/views.py
+++ app/views.py
@@ -3,11 +3,10 @@
 from app import app, db
 from flask import render_template, request, redirect, url_for, session, flash
 from flask_login import login_required, login_user, current_user, logout_user
-from werkzeug.security import check_password_hash
 from .models import User
 import sqlalchemy as sql
-from .forms import LoginForm
-from .services import run_scanner_service
+from .forms import LoginForm, ScanForm
+from .services import run_service
 
 
 # Базовая страница
@@ -16,16 +15,17 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/user/")
+@app.route("/profile/")
+@login_required
 def profile():
     return render_template("profile.html")
 
 
 # Логин
-@app.route("/user/login/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("scan"))
+        return redirect(url_for("profile"))
 
     form = LoginForm()
     if form.validate_on_submit():
@@ -41,7 +41,7 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
-@app.route("/user/registration", methods=["GET", "POST"])
+@app.route("/registration", methods=["GET", "POST"])
 def registration():
     if request.method == "POST":
         username = request.form["username"]
@@ -65,45 +65,54 @@ def registration():
         return redirect("/")
     return render_template("register.html")
 
-
 # Сам сканнер
-@app.route("/user/scan", methods=["POST"])
+@app.route("/profile/scan", methods=["POST", 'GET'])
 @login_required  # проверка на авторизацию
 def scan():
     # запуск сканера и ввод
+    form = ScanForm()
+    
     try:
-        scanner = run_scanner_service()
-        code = request.form.get("code")
-        repo = request.form.get("repo_url")
-        # проверка входных данных
-        if code:
-            result = scanner.run_code_scan(code)
-        elif repo:
-            result = scanner.run_repo_scan(repo)
-        else:
-            flash("Укажите код или URL репозитория на GitHub")
-            return redirect(url_for("index"))
-        return render_template("results.html", result=result)
+        if request.method == "POST":
+            scanner = run_service()
+            code = request.form.get("code", "").strip()
+            repo_url = request.form.get("repo", "").strip()
+            
+            # проверка входных данных
+            if code:
+                result = scanner.run_code_scan(code)
+            elif repo_url:
+                result = scanner.run_repo_scan(repo_url)
+            else:
+                flash("Укажите код или URL репозитория на GitHub")
+                return render_template('scan.html', form=form)
+            
+            # проверка наличия результата
+            if not result or not isinstance(result, dict):
+                flash("Результаты отсутствуют.")
+                return render_template('scan.html', form=form)
 
-    # если ошибка то кинет на index
+            return render_template("results.html", result=result)
+
+    # проверка ошибки, перенаправляет обратно на скан
     except ValueError as e:
         flash(f"Ошибка конфигурации: {str(e)}")
-        return redirect(url_for("index"))
+        return render_template('scan.html', form=form)
     except RuntimeError as e:
         flash(f"Ошибка сканирования: {str(e)}")
-        return redirect(url_for("index"))
+        return render_template('scan.html', form=form)
     except Exception as e:
         flash(f"Ошибка: {str(e)}")
-        return redirect(url_for("index"))
-
+        return render_template('scan.html', form=form)
+    return render_template('scan.html', form=form)
 
 # Результаты скана
-@app.route("/user/results", methods=["POST"])
+@app.route("/profile/results", methods=["POST"])
 def results():
     return "Results page"
 
 
-@app.route("/user/logout")
+@app.route("/profile/logout")
 @login_required
 def logout():
     logout_user()
