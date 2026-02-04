deaf115c38edfaa326e46463aac548a3ccc60d91
Binary101010 <gosling712@gmail.com>
2026-02-04 21:31:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #Импорты
 from os import*
 
-#Находит директорию проекта, нужно для того чтобы конфиг применился
+#Находит директорию проекта
 app_dir = path.abspath(path.dirname(__file__))
 
 
@@ -10,7 +10,6 @@ class BaseConfig:
     SECRET_KEY: str = r'XP;W/\vD*BOQ_ieHYfEl1GJ!2}Z5[?S:A.7ykd6nL3c8qwpN+$bjt])MV#0z'
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     
-#! Конфиг для разработки, делитнуть на релизе!
 class DevelopmentConfig(BaseConfig):
     DEBUG: bool = False
     SQLALCHEMY_DATABASE_URI: str = 'sqlite:///default.db'
