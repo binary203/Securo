08db02d55f44e07d3c1e2cf59a2c08eef1013f8c
Binary101010 <gosling712@gmail.com>
2026-02-06 19:30:31 +0300

    Remove SECRET_KEY from config

--- config.py
+++ config.py
@@ -20,6 +20,5 @@ class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI: str = os.environ.get(
         "DATABASE_URL", "sqlite:///production.db"
     )
-    SECRET_KEY: str = os.environ.get("SECRET_KEY", BaseConfig.SECRET_KEY)
     SEMGREP_API_TOKEN: str = os.environ.get("SEMGREP_APP_TOKEN", "")
     GEMINI_API: str = os.environ.get("GEMINI_API_KEY", "")
