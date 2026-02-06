05258b6bde371f1bdbb6898e04ff9802b8a6cf0d
Binary101010 <gosling712@gmail.com>
2026-02-06 19:24:21 +0300

    Refactor config.py for environment variable usage
    Updated import statements and modified configuration settings to use environment variables for production.

--- config.py
+++ config.py
@@ -1,16 +1,25 @@
-#Импорты
-from os import*
+# Импорты
+import os
 
-#Находит директорию проекта
-app_dir = path.abspath(path.dirname(__file__))
+# Находит директорию проекта, нужно для того чтобы конфиг применился
+app_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-#Базовый конфиг
+# Базовый конфиг
 class BaseConfig:
-    SECRET_KEY: str = r'XP;W/\vD*BOQ_ieHYfEl1GJ!2}Z5[?S:A.7ykd6nL3c8qwpN+$bjt])MV#0z'
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
-    
+
+
 class DevelopmentConfig(BaseConfig):
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///default.db"
+
+
+class ProductionConfig(BaseConfig):
     DEBUG: bool = False
-    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///default.db'
-    SEMGREP_API_TOKEN=''
+    SQLALCHEMY_DATABASE_URI: str = os.environ.get(
+        "DATABASE_URL", "sqlite:///production.db"
+    )
+    SECRET_KEY: str = os.environ.get("SECRET_KEY", BaseConfig.SECRET_KEY)
+    SEMGREP_API_TOKEN: str = os.environ.get("SEMGREP_APP_TOKEN", "")
+    GEMINI_API: str = os.environ.get("GEMINI_API_KEY", "")
